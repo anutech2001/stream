93f4ee87d284594ca956db2fcdc2dc3211327380
Anurak Th <anutech2001@gmail.com>
2021-02-19 15:16:16 +0700

    adjust demo.

--- src/main/java/com/example/stream/PrimitiveStreamExample.java
+++ src/main/java/com/example/stream/PrimitiveStreamExample.java
@@ -13,28 +13,18 @@ public class PrimitiveStreamExample {
         Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
         System.out.println(stream.reduce(0, (s, n) -> s + n)); // 15
         //// Using IntStream
-        Stream<Integer> intStream = Stream.of(1, 2, 3, 4, 5);
-        System.out.println(intStream.mapToInt(x -> x).sum()); // 15
+        Stream<Integer> stream2 = Stream.of(1, 2, 3, 4, 5);
+        System.out.println(stream2.mapToInt(x -> x).sum()); // 15
 
         //// Compute an average
         IntStream intStream1 = IntStream.of(1, 2, 3, 4, 5);
         OptionalDouble avg = intStream1.average();
         System.out.println(avg.getAsDouble()); // 3.0
 
-        Stream<String> objStream = Stream.of("penguin", "fish");
-        IntStream intStream2 = objStream.mapToInt(s -> s.length());
-
-        threeDigit(Optional.empty());
-        threeDigit(Optional.of(433));
         var ohMy = Stream.of("lions", "tigers", "bears");
         Double result = ohMy.collect(Collectors.averagingInt(String::length));
         System.out.println(result); // 5.333333333333333
 
     }
 
-    private static void threeDigit(Optional<Integer> optional) {
-        optional.map(n -> "" + n) // part 1
-                .filter(s -> s.length() == 3) // part 2
-                .ifPresent(System.out::println); // part 3
-    }
 }
